df3f2aac6c958688cdeba2d8104be4a86d3073c9
Aubbakr <realchampiontheno1@gmail.com>
2019-09-06 12:49:29 +0500

    Updates

---
+++ precache-manifest.754a7e5ba6268e973f2439e160bcfa4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbac0010ee743377fb38681a9353b556",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e5f58b86a2b5b89a4764",
+    "url": "/monsters-rolodex/static/css/main.4e1a3241.chunk.css"
+  },
+  {
+    "revision": "86a670949e2d766ed922",
+    "url": "/monsters-rolodex/static/js/2.428fc73a.chunk.js"
+  },
+  {
+    "revision": "e5f58b86a2b5b89a4764",
+    "url": "/monsters-rolodex/static/js/main.01de165b.chunk.js"
+  },
+  {
+    "revision": "4e8a8e8c497e779916d5",
+    "url": "/monsters-rolodex/static/js/runtime~main.fb49a3bc.js"
+  }
+]);
